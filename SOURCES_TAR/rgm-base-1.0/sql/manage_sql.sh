c8bc14355497844b92dfb4cc5df1d77476f848b1
Eric Belhomme <ebelhomme@ricozome.net>
2019-03-20 14:14:04 +0100

    fix sql cnx timeout because for mysqld startup delay

--- SOURCES_TAR/rgm-base-1.0/sql/manage_sql.sh
+++ SOURCES_TAR/rgm-base-1.0/sql/manage_sql.sh
@@ -7,7 +7,7 @@
 PATH="/sbin:/usr/sbin:/bin:/usr/bin"
 
 function help () {
-    cat <<EOF
+  cat <<EOF
 
 $0 - install or update MariaDB SQL schema
 
@@ -48,15 +48,15 @@ done
 
 
 if [ -z $DBNAME ]; then
-    # database name not provided
-    exit 1
+  # database name not provided
+  exit 1
 fi
 
 if [ ! -z $FILE_SQL_SCHEMA ]; then
-    if [ ! -e $FILE_SQL_SCHEMA ]; then
-        # SQL schema file not found
-        exit 3
-    fi
+  if [ ! -e $FILE_SQL_SCHEMA ]; then
+    # SQL schema file not found
+    exit 3
+  fi
 fi
 
 MYSQL_SOCKET=$(mktemp -u --suffix -mysql-sock)
@@ -68,33 +68,62 @@ systemctl stop mariadb
 /usr/bin/mysqld_safe --skip-grant-tables --no-auto-restart --skip-networking --socket=${MYSQL_SOCKET} &> /dev/null
 MYSQL_PID=$(ps aux | grep '^mysql .*[m]ysqld' | awk '{print $2}')
 if [ -z $MYSQL_PID ]; then
-    # failed to start mariadb in safe mode
-    systemctl start mariadb
-    exit 1
+  # failed to start mariadb in safe mode
+  systemctl start mariadb
+  exit 1
 fi
+
+# wait for the mysqld socket ready to handle SQL cnx
+COUNT=0
+while : ; do
+  $MYSQL -e '\q' &> /dev/null
+  if [ $? -eq 0 ]; then
+    break;
+  fi
+  COUNT=$(( $COUNT +1 ))
+  if [ $COUNT -gt 10 ]; then
+    echo "Failed to connect mysqld_safe"
+    exit 1
+  fi
+  sleep 1
+done
 
 # create and init database if it not already exists
 if [ "$($MYSQL -e 'show databases' | grep -c ^${DBNAME}\$)" == "0" ]; then
-    $MYSQL -e "CREATE DATABASE ${DBNAME};"
-    if [ ! -z $FILE_SQL_SCHEMA ]; then
-        $MYSQL ${DBNAME} < ${FILE_SQL_SCHEMA}
-    fi
+  $MYSQL -e "CREATE DATABASE ${DBNAME};"
+  if [ ! -z $FILE_SQL_SCHEMA ]; then
+    $MYSQL ${DBNAME} < ${FILE_SQL_SCHEMA}
+  fi
 fi
 
 # is a user is supplied, grant privileges on DB to that user
 if [ ! -z $SQL_USER ]; then
-    USERNAME=$(echo $SQL_USER | cut -d '@' -f 1)
-    USERHOST=$(echo $SQL_USER | cut -d '@' -f 2)
-    if [ -z $USERHOST ]; then USERHOST='localhost'; fi
-    if [ -z $SQL_PASSWORD ]; then
-        $MYSQL -e "CREATE USER IF NOT EXISTS ${USERNAME};"
-    else
-        $MYSQL -e "CREATE USER IF NOT EXISTS ${USERNAME} IDENTIFIED BY '${SQL_PASSWORD}';"
-    fi
-    $MYSQL -e "GRANT ${SQL_PRIVILEGES} ON `${DBNAME}`.* TO '${USERNAME}'@'${USERHOST}';"
+  USERNAME=$(echo $SQL_USER | cut -d '@' -f 1)
+  USERHOST=$(echo $SQL_USER | cut -d '@' -f 2)
+  if [ -z $USERHOST ]; then USERHOST='localhost'; fi
+  if [ -z $SQL_PASSWORD ]; then
+    $MYSQL -e "CREATE USER IF NOT EXISTS ${USERNAME};"
+  else
+    $MYSQL -e "CREATE USER IF NOT EXISTS ${USERNAME} IDENTIFIED BY '${SQL_PASSWORD}';"
+  fi
+  $MYSQL -e "GRANT ${SQL_PRIVILEGES} ON `${DBNAME}`.* TO '${USERNAME}'@'${USERHOST}';"
 fi
 
 # terminate safe instance and resume normal mariadb instance
 kill -15 $MYSQL_PID
+COUNT=0
+while : ; do
+  sleep 1
+  ps aux | grep '^mysql .*[m]ysqld' &> /dev/null
+  if [ $? -eq 1 ]; then
+    break;
+  fi
+  COUNT=$(( $COUNT +1 ))
+  if [ $COUNT -gt 10 ]; then
+    kill -9 $MYSQL_PID
+    sleep 2
+    break
+  fi
+done
 systemctl start mariadb
 exit 0
