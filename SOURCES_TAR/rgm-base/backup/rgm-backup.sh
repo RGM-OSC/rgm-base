58807ddcf41350a3eb503697fd128c7c3ee5ba30
Eric Belhomme <ebelhomme@fr.scc.com>
2021-05-04 05:02:15 +0000

    fixes & enhancements:

---
+++ SOURCES_TAR/rgm-base/backup/rgm-backup.sh
@@ -0,0 +1,103 @@
+#!/bin/bash -e
+#
+# RGM platform backup script using restic solution
+#
+# forked from https://github.com/vfricou/script-rgm-restic-backup
+#
+# Vincent FRICOU <vincent@fricouv.eu> 2020
+# SCC France 2020 - Eric Belhomme <ebelhomme@fr.scc.com>
+
+# User definitions
+BACKUP_ROOT='/srv/rgm/backup'
+BACKUP_PATH="${BACKUP_ROOT}/restic"
+RESTICRETENTION='-d 7 -w 4 -m 3'
+RESTICBIN='/usr/bin/restic'
+RESTICPWDLEN='110'
+RESTICPWDFILE='/root/.resticpwd'
+MARIADBCLIENTCNF='/root/.my.cnf'
+BACKUPPATHLIST=('/etc' '/srv' '/var' "$RESTICPWDFILE" "$MARIADBCLIENTCNF")
+
+NOW="$(date +"%Y-%m-%d_%H%M")"
+LOGFILE="${BACKUP_ROOT}/logs/${NOW}.log"
+if [ ! -d "$(dirname "$LOGFILE")" ]; then
+    mkdir -p "$(dirname "$LOGFILE")"
+fi
+touch "$LOGFILE" && chmod 0640 "$LOGFILE" || exit 1
+
+
+function log_tee() {
+    echo "-----------------------------------------------------------" | tee -a "${LOGFILE}"
+    echo -e "# $1\n" | tee -a "${LOGFILE}"
+}
+
+function create_clean_target() {
+    if [ -d "${1}" ]; then
+        rm -rf "${1:?}/"*
+    else
+        mkdir -p "${1}"
+    fi
+}
+
+cat <<EOF | tee "${LOGFILE}"
+# $(basename "$0") started at $(date +"%d %b %Y - %H:%M:%S")
+-----------------------------------------------------------
+
+EOF
+
+# generate and secure a random password if none exists yet
+if [ ! -d ${BACKUP_PATH}/index ]; then
+    echo "Generating restic repository password in ${RESTICPWDFILE} file"
+    < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c"${1:-${RESTICPWDLEN}}" > ${RESTICPWDFILE}
+    chmod 0400 "${RESTICPWDFILE}"
+    ${RESTICBIN} --repo ${BACKUP_PATH} -p ${RESTICPWDFILE} init
+fi
+
+# prune old restic snapshots
+log_tee "Start backup retention cleaning (with retention ${RESTICRETENTION})"
+${RESTICBIN} --repo ${BACKUP_PATH} -p ${RESTICPWDFILE} forget --keep-daily ${RESTICRETENTION} --prune | tee -a "${LOGFILE}"
+log_tee "End backup retention cleaning"
+
+# dump & backup mariadb databases
+log_tee "Starting mysql dumps"
+BACKUPTARGET="${BACKUP_ROOT}/dumps/mariadb"
+create_clean_target "$BACKUPTARGET"
+for db in $(mysql --defaults-extra-file=${MARIADBCLIENTCNF} -Bse 'show databases' | grep -Pv 'information_schema|performance_schema|mysql'); do
+    File="${BACKUPTARGET}/${db}.sql"
+    echo "Dumping database ${db}" | tee -a "${LOGFILE}"
+    mysqldump --defaults-extra-file=${MARIADBCLIENTCNF} --compact --order-by-primary --add-drop-table "${db}" -R 2>> "${LOGFILE}" > "$File"
+done
+log_tee "Upload mariadb dumps into restic target"
+$RESTICBIN --repo $BACKUP_PATH -p $RESTICPWDFILE backup "$BACKUPTARGET" | tee -a "${LOGFILE}"
+
+# dump & backup influxdb databases
+log_tee "Starting influxdb dumps"
+BACKUPTARGET="${BACKUP_ROOT}/dumps/influxdb"
+create_clean_target "$BACKUPTARGET"
+for db in $(influx -precision rfc3339 -execute 'show databases' | grep -ve '^name$' -ve 'name: databases' -ve '----'); do
+    Folder="${BACKUPTARGET}/${db}"
+    echo "Dumping database ${db}" | tee -a "${LOGFILE}"
+    influxd backup -database "${db}" "${Folder}" 1>> "${LOGFILE}" 2>/dev/null
+done
+log_tee "Upload influxdb dumps into restic target start"
+${RESTICBIN} --repo $BACKUP_PATH -p $RESTICPWDFILE backup "$BACKUPTARGET" | tee -a "${LOGFILE}"
+
+# backup flat filesystem
+log_tee "Start flat filesystem backup"
+echo -e "\nBackup folders ${BACKUPPATHLIST[*]}" | tee -a "${LOGFILE}"
+${RESTICBIN} --repo ${BACKUP_PATH} -p ${RESTICPWDFILE} \
+    --exclude ${BACKUP_ROOT} \
+    --exclude /var/lib/elasticsearch \
+    --exclude /var/lib/mysql \
+    --exclude /var/lib/influxdb \
+    --exclude "/srv/rgm/nagios*/var/log/spool" \
+    --exclude "/srv/rgm/thruk*/var/sessions" \
+    --exclude "/srv/rgm/nagios*/etc/lilac-backup*" \
+    --exclude /srv/rgm/backup \
+    backup "${BACKUPPATHLIST[@]}" | tee -a "${LOGFILE}"
+
+cat <<EOF | tee "${LOGFILE}"
+
+-----------------------------------------------------------
+# $(basename "$0") finished at $(date +"%d %b %Y - %H:%M:%S")
+EOF
+# vim: expandtab sw=4 ts=4:
